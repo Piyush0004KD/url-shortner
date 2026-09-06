328ec3dc218e399c9c1e32a558f20ccacbb2c766
PiyushKD <piyushkd15@gmail.com>
2026-09-07 02:39:25 +0530

    added the entity and packages

--- src/main/java/com/PiyushKD/urlShortner/entity/url.java
+++ src/main/java/com/PiyushKD/urlShortner/entity/url.java
@@ -1,20 +1,36 @@
 package com.PiyushKD.urlShortner.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 @Data
 @Builder
-@RequiredArgsConstructor
+@AllArgsConstructor
 @NoArgsConstructor
+@Table(name="url")
 public class url {
 
     @Id
-    @Generated()
-    private int id;
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_seq"
+    )
+    @SequenceGenerator(
+            name="user_seq",
+            sequenceName = "user_id_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    @NotBlank
+    private String longUrl;
+
+    @NotBlank
+    private String shortUrl;
 
 
 }
