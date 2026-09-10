1d663645468c597f0d22c44aa718b7c1fdb8d58d
PiyushKD <piyushkd15@gmail.com>
2026-09-10 17:04:13 +0530

    add pagenation

--- src/main/java/com/PiyushKD/urlShortner/urlController/UrlController.java
+++ src/main/java/com/PiyushKD/urlShortner/urlController/UrlController.java
@@ -5,12 +5,13 @@ import com.PiyushKD.urlShortner.dto.UrlResponse;
 import com.PiyushKD.urlShortner.entity.Url;
 import com.PiyushKD.urlShortner.urlService.UrlService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping
@@ -35,8 +36,8 @@ public class UrlController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Url>> getAllUrls() {
-        return ResponseEntity.ok(urlService.getAll());
+    public ResponseEntity<Page<Url>> getAllUrls(Pageable pageable) {
+        return ResponseEntity.ok(urlService.getAll(pageable));
     }
 
 
